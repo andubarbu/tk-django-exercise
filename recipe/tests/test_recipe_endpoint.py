4b1516e98ea1dbad70d7afbbf422dbf20f4b475c
Andrei <andrei.barbu@travelperk.com>
2021-07-15 15:38:40 +0200

    sorted response list in test_retrieve_recipes

--- recipe/tests/test_recipe_endpoint.py
+++ recipe/tests/test_recipe_endpoint.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from rest_framework.test import APIClient
 from core.models import Ingredient, Recipe
 from recipe.serializers import RecipeSerializer
-import json
+import operator
 
 
 RECIPES_URL = reverse('recipe:recipe-list')
@@ -56,8 +56,9 @@ class RecipeApiTests(TestCase):
             ]
         }
         response = res.json()
+        sorted_json = sorted(response, key=lambda x: x["id"])
         self.assertEqual(res.status_code, status.HTTP_200_OK)
-        self.assertEqual(response, expected_data['data'])
+        self.assertEqual(sorted_json, expected_data['data'])
 
     def test_retrieve_recipe_detail(self):
         """Retrieving a specific recipe"""
